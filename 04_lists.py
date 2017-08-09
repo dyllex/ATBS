dcfb137e1733e8289b0ad256e44cda58f6c6f2bd
Dylan Yokoyama <amayokoy2@gmail.com>
2017-08-09 16:03:17 -0700

    augmented operators

--- 04_lists.py
+++ 04_lists.py
@@ -118,3 +118,25 @@ cat = ['fat', 'orange', 'loud']
 size, color, disposition = cat
 
 
+"""
+Augmented assignment operators
+"""
+# spam += 1
+# spam = spam + 1
+
+# spam -= 1
+# spam = spam - 1
+
+# spam *= 1
+# spam = spam * 1
+
+# spam /= 1
+# spam = spam / 1
+
+# spam %= 1
+# spam = spam % 1
+
+
+"""
+Methods
+"""
